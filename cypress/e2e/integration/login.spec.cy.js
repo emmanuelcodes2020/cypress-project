6c8c857bcd2bf5b5c792b53c3b33086278f014aa
Emmanuel Yewande <emmanuel.yewande@outlook.com>
2025-02-16 15:35:41 +0000

    Initial Cypress Set Up

---
+++ cypress/e2e/integration/login.spec.cy.js
@@ -0,0 +1,14 @@
+
+describe('Login Test', () => {
+  it('should log in successfully with valid credentials', () => {
+    cy.visit('https://www.saucedemo.com/'); // Visit login page
+
+    cy.get('#user-name').type('standard_user'); // Enter username
+    cy.get('#password').type('secret_sauce');   // Enter password
+    cy.get('#login-button').click();           // Click login button
+
+    // Assertion: Verify successful login by checking if the inventory page is visible
+    cy.url().should('include', '/inventory');
+    cy.get('.inventory_list').should('be.visible');
+  });
+});
